cbdc6738bec2b920b95f952d39d41e9b4bc0d4b8
yabon-exe <yabon116@gmail.com>
2024-07-25 17:46:01 +0900

    U

--- godot-mobile/tate_golf.service.worker.js
+++ godot-mobile/tate_golf.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1721405400|445457280953';
+const CACHE_VERSION = '1721897119|147438783339';
 /** @type {string} */
 const CACHE_PREFIX = 'tate_golf-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
