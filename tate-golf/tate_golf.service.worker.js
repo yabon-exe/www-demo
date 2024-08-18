cadfd948000d6a9ca29012a95fbf74d8453c8b1b
yabon_exe <yabon116@gmail.com>
2024-08-18 20:05:55 +0900

    U

--- tate-golf/tate_golf.service.worker.js
+++ tate-golf/tate_golf.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1723914184|4974662166';
+const CACHE_VERSION = '1723979082|86543272';
 /** @type {string} */
 const CACHE_PREFIX = 'tate_golf-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
