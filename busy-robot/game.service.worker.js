53e04ac54f1d774ab14f1ed25b5d6a6cf5b9f46d
yabon_exe <yabon116@gmail.com>
2024-12-14 14:38:15 +0900

    X

--- busy-robot/game.service.worker.js
+++ busy-robot/game.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734151454|479078636';
+const CACHE_VERSION = '1734154599|3623664021';
 /** @type {string} */
 const CACHE_PREFIX = 'move_shooting-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
